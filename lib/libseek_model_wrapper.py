d588af51111f1c84f165ac24f83af2eb57bf2724
Bobby-Ling <bobby-ling@outlook.com>
2024-09-22 22:04:57 +0800

    update c .so wrapper

--- lib/libseek_model_wrapper.py
+++ lib/libseek_model_wrapper.py
@@ -2,7 +2,8 @@ import ctypes
 from ctypes import *
 
 # 加载共享库
-lib = ctypes.CDLL('./libseek_model.so')
+# ctypes.RTLD_GLOBAL 在加载 main.so 时, 可以访问之前加载的其他库中的符号
+lib = ctypes.CDLL('./libseek_model.so', mode=ctypes.RTLD_GLOBAL)
 
 # 最大wrap
 MAX_WRAP = 280
@@ -125,6 +126,7 @@ lib.TotalMotorWearTimes.restype = c_uint32
 
 # 使用类型注解添加函数封装
 
+import os
 from typing import Union
 
 def seek_time_calculate(start: HeadInfo, target: HeadInfo) -> int:
@@ -154,3 +156,61 @@ def total_tape_belt_wear_times(input_param: InputParam, output_param: OutputPara
 def total_motor_wear_times(input_param: InputParam, output_param: OutputParam) -> int:
     """统计电机磨损次数"""
     return lib.TotalMotorWearTimes(byref(input_param), byref(output_param))
+
+lib_main = ctypes.CDLL('./libproject_hw_dl.so')
+
+# 定义包装函数
+def parse_file(filename: str, head_info: HeadInfo, io_vector: IOVector) -> int:
+    """调用 C 库中的 parseFile 函数，并传入文件名、HeadInfo 和 IOVector"""
+    filename_bytes = filename.encode('utf-8')  # 将文件名转换为字节字符串
+    result = lib_main.parseFile(c_char_p(filename_bytes), byref(head_info), byref(io_vector))
+    return result
+
+# 定义Context结构体
+class Context(Structure):
+    _fields_ = [
+        ("input", POINTER(InputParam))  # 定义结构体字段
+    ]
+
+    def __init__(self, input_param: InputParam):
+        # 调用父类的初始化方法
+        super().__init__()
+
+        # 将传入的 InputParam 实例的指针赋值给 input 字段
+        self.input = pointer(input_param)
+
+# 设置getNodeDist函数参数类型和返回类型
+lib_main.getNodeDist.argtypes = [c_uint32, c_uint32, POINTER(Context)]
+lib_main.getNodeDist.restype = c_uint32
+
+# 包装getNodeDist函数
+def get_node_dist(idx_from: int, idx_to: int, ctx: Context) -> int:
+    """调用 C 库中的 getNodeDist 函数"""
+    result = lib_main.getNodeDist(c_uint32(idx_from), c_uint32(idx_to), byref(ctx))
+    return result
+
+# 设置getDistMatrix函数参数类型和返回类型
+lib_main.getDistMatrix.argtypes = [POINTER(InputParam), POINTER(c_int)]
+lib_main.getDistMatrix.restype = None  # void 函数无返回值
+
+# 包装getDistMatrix函数
+def get_dist_matrix(input_param: InputParam, matrix_2d: ctypes.Array) -> None:
+    """调用 C 库中的 getDistMatrix 函数"""
+    lib_main.getDistMatrix(byref(input_param), matrix_2d)
+
+
+# os.chir("")
+
+# 使用示例
+if __name__ == "__main__":
+    head_info = HeadInfo()
+    io_vector = IOVector()
+
+    result = parse_file("../dataset/case_1.txt", head_info, io_vector)
+
+    if result == 0:
+        print(f"File parsed successfully. HeadInfo: wrap={head_info.wrap}, lpos={head_info.lpos}, status={head_info.status}")
+        print(f"IOVector length: {io_vector.len}")
+        print(f"IOVector[0].id : {io_vector.ioArray[0].id}")
+    else:
+        print("Error parsing file.")
