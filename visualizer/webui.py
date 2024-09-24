e505fdbb7f984b59223fbead893ea698c3f530b3
Bobby-Ling <bobby-ling@outlook.com>
2024-09-24 11:03:13 +0800

    在visualizer目录下运行

--- visualizer/webui.py
+++ visualizer/webui.py
@@ -1,7 +1,14 @@
+# %%
 import http.server
+import os
+from pathlib import Path
 import socketserver
 import webbrowser
 
+# %%
+file_dir = Path(__file__).parent
+
+# %%
 def start_server(tracing_file:str = "profiling.json", port:int = 8000, open_in_browser:bool =False):
     def start_srv_internal(tracing_file:str = "profiling.json", port:int = 8000, open_in_browser:bool =False):
         # 服务器端口
@@ -15,8 +22,8 @@ def start_server(tracing_file:str = "profiling.json", port:int = 8000, open_in_b
             print(f"Serving at port {PORT}")
 
             # 打开默认浏览器并加载tracing.html，同时传递参数
-            url = f"http://localhost:{PORT}/tracing.html?tracing_url={tracing_file}"
-            print(f"url: {url}")
+            url = f"http://localhost:{PORT}/tracing.html"
+            print(f"url: {url}?tracing_url={tracing_file}")
 
             if open_in_browser:
                 webbrowser.open(url)
@@ -33,3 +40,10 @@ def start_server(tracing_file:str = "profiling.json", port:int = 8000, open_in_b
         except:
             PORT += 1
             continue
+
+# %%
+if __name__ == '__main__':
+    # 命令行运行时执行, import导入时不执行
+    os.chdir(file_dir / '../build/')
+    start_server('profiling.json')
+# %%
