1117ba8cbb25257af0d450543453350a15bacf97
Bobby-Ling <bobby-ling@outlook.com>
2024-10-18 09:47:46 +0800

    result_10-1000-10_SCAN_Greedy_LKHembeded_LNS_4.png

--- lib/score_system.py
+++ lib/score_system.py
@@ -377,18 +377,26 @@ def run_test():
         # METHOD.LKH,
         # METHOD.LKH1,
         METHOD.LNS,
+        METHOD.LKH_embeded,
     ]
     # 考虑一般情况，io在前100%，随机分布，长度随机
     io_counts: list[int] = np.concatenate(
         [
-            # np.arange(10, 1000, 10),
-            np.arange(1000, 2000, 100),
-            np.arange(2000, 5000, 500),
-            np.arange(5000, 10001, 1000),
+            np.arange(10, 1000, 10),
+            # np.arange(1000, 2000, 100),
+            # np.arange(2000, 5000, 500),
+            # np.arange(5000, 10001, 1000),
         ]
     ).tolist()
     # io_counts: list[int] = [10, 50, 100, 1000, 5000, 10000]
     results = run_method_in_batch(methods, io_counts)
+    for method, method_result in results.items():
+        print("\n----------------------------------------------------")
+        print(
+            f"{method.name} 算法总分为{sum(io_result.score for io_result in method_result.values())}"
+        )
+        print("----------------------------------------------------\n")
+
     visualize_results(results, io_counts)
 
 
